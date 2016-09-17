256b23151534dcf3ff14116bef4d39774266aab0
jeffthemaximum <frey.maxim@gmail.com>
2016-09-17 19:01:35 -0400

    makes new scrape model

--- scraper/views.py
+++ scraper/views.py
@@ -3,13 +3,17 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from scraper.tasks import scrape_and_update_sheet_task
 from channels import Channel
+from scraper.models import Scrape
 import pudb
 
 # Create your views here.
 @csrf_exempt
 def new(request):
+    scrape = Scrape(name=request.POST.get('name'))
+    scrape.save()
     name = {
-        'sheet_name': request.POST.get('name')
+        'sheet_name': scrape.name,
+        'sheet_id': scrape.id
     }
     Channel('scrape_wiley_by_sheet_name').send(name)
     return HttpResponse('hello')
